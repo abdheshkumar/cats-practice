8ed51781ada91cbd1d1e3ce8f2d4046718aee19b
abdheshkumar <abdhesh@callhandling.co.uk>
2018-09-24 15:29:21 +0100

    Fixed stackover flow exception

--- src/test/scala/TreeSpec.scala
+++ src/test/scala/TreeSpec.scala
@@ -9,16 +9,19 @@ class TreeSpec extends CatsSuite {
 
     def genLeaf[A: Arbitrary]: Gen[Leaf[A]] = arbitrary[A].map(Leaf(_))
 
-    def genNode[A: Arbitrary]: Gen[Branch[A]] =
+    def genNode[A: Arbitrary](level: Int): Gen[Branch[A]] =
       for {
-        left  <- genTree[A]
-        right <- genTree[A]
+        left  <- genTree[A](level)
+        right <- genTree[A](level)
       } yield Branch(left, right)
 
-    def genTree[A: Arbitrary]: Gen[Tree[A]] = Gen.oneOf(Gen.lzy(genLeaf[A]), Gen.lzy(genNode[A]))
+    def genTree[A: Arbitrary](level: Int): Gen[Tree[A]] =
+      if (level >= 100) {
+        genLeaf[A]
+      } else Gen.oneOf(Gen.lzy(genLeaf[A]), Gen.lzy(genNode[A](level + 1)))
 
     implicit def arbTree[A: Arbitrary]: Arbitrary[Tree[A]] =
-      Arbitrary(genNode[A])
+      Arbitrary(genNode[A](level = 0))
 
   }
   import TreeArbitrary._
